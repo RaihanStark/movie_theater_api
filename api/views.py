996bc0607e19df2d412c75620bda971e11bf8d5c
Raihan Stark <raihanyudosaputra30@gmail.com>
2020-09-17 21:37:19 +0700

    add filter

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 
 from .serializers import MovieSerializer, TheaterSerializer, ShowtimeSerializer
 from .models import Movie, Theater, Showtime
@@ -7,10 +7,14 @@ from .models import Movie, Theater, Showtime
 # Create your views here.
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all().order_by('id')
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
     serializer_class = MovieSerializer
 
 class TheaterViewSet(viewsets.ModelViewSet):
     queryset = Theater.objects.all().order_by('id')
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
     serializer_class = TheaterSerializer
 
 class ShowtimeViewSet(viewsets.ModelViewSet):
